3f71df250cd7b0788ecb665fa89f3ea1e97e34db
Anadi Sharma <1anadi1sharma@gmail.com>
2026-07-29 01:52:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (54.14%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(int i = 1; i <= num/i ; i++){
+            if(i*i == num){
+                return true;
+            }
+        }
+        return false;
+
+        
+    }
+};
